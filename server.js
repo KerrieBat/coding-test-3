bfc6ef24d2e1494ae12346d080bd6d54045d9292
KerrieBat <batrouney@gmail.com>
2017-03-04 13:38:51 +1100

    basic structure

--- server.js
+++ server.js
@@ -1,39 +1,74 @@
 
-var express = require('express');
-var app = express();
-// app.get('/', function (req, res) {
-// res.send('<h1>Open Source For You!</h1>');
-// });
-// app.listen(3000, function () {
-// console.log('Example app listening on port 3000!');
-// });
+var express = require('express')
+var app = express()
+var bodyParser= require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
 
 var router = express.Router();
-var path = __dirname + '/views/';
+// var path = __dirname + '/views/';
 
 router.use(function (req,res,next) {
   console.log('/' + req.method);
   next();
 });
 
-router.get('/',function(req,res){
-  res.sendFile(path + 'index.html');
-});
+// router.get('/',function(req,res){
+//   res.render('/pages/index')
+// });
 
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
 
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});
 
-app.use("/",router);
 
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
-});
+//home page
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about')
+})
+
+// contact page
+app.get('/contact', (req, res) => {
+    res.render('pages/contact')
+})
+
+// FAQ page
+app.get('/FAQ', (req, res) => {
+    res.render('pages/FAQ')
+})
+
+// Projects page
+app.get('/projects', (req, res) => {
+    res.render('pages/projects')
+})
+
+// Volunteering page
+app.get('/volunteer', (req, res) => {
+    res.render('pages/volunteer')
+})
+
+
+//
+// router.get("/about",function(req,res){
+//   res.sendFile(path + "about.html");
+// });
+//
+// router.get("/contact",function(req,res){
+//   res.sendFile(path + "contact.html");
+// });
+//
+// app.use("/",router);
+//
+// app.use("*",function(req,res){
+//   res.sendFile(path + "404.html");
+// });
 
 app.listen(3000,function(){
   console.log('Listening on Port 3000');
